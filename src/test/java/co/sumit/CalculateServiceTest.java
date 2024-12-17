0bebce35b5ddac2a0b679e1c114c3577380e251e
sumitbasu116@gmail.com <sumitbasu116@gmail.com>
2024-12-18 00:44:46 +0530

    before-class

--- src/test/java/co/sumit/CalculateServiceTest.java
+++ src/test/java/co/sumit/CalculateServiceTest.java
@@ -1,9 +1,19 @@
 package co.sumit;
 
+import java.util.Date;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculateServiceTest {
+	
+	//Method init should be static
+	@BeforeClass
+	public static void init() {
+		System.out.println("Before all test cases...");
+		System.out.println("Started test at : "+new Date());
+	}
 	
 	@Test
 	public void addTwoNumbersTest() {
